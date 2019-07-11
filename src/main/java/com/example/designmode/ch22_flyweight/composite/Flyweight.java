5e631ec8892d41ebf68ceecd37157c60dbb8c386
qlong8807 <qlong8807@hotmail.com>
2019-07-11 12:49:46 +0800

    享元模式

---
+++ src/main/java/com/example/designmode/ch22_flyweight/composite/Flyweight.java
@@ -0,0 +1,6 @@
+package com.example.designmode.ch22_flyweight.composite;
+
+public interface Flyweight {
+    //一个示意性方法，参数state是外蕴状态
+    public void operation(String state);
+}
---
+++ src/main/java/com/example/designmode/ch22_flyweight/simple/Client22s.java
@@ -0,0 +1,22 @@
+package com.example.designmode.ch22_flyweight.simple;
+
+
+/**
+ * 虽然客户端申请了三个享元对象，但是实际创建的享元对象只有两个，这就是共享的含义。
+ */
+public class Client22s {
+
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        FlyweightFactory factory = new FlyweightFactory();
+        Flyweight fly = factory.factory(new Character('a'));
+        fly.operation("First Call");
+
+        fly = factory.factory(new Character('b'));
+        fly.operation("Second Call");
+
+        fly = factory.factory(new Character('a'));
+        fly.operation("Third Call");
+    }
+}
